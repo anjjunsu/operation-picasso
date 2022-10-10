81c0a1683d6b4837abe7cc5e79a7b52621945bce
Junsu An <anjjunsu@gmail.com>
2022-10-10 16:32:09 -0700

    Basic canvas

--- app.js
+++ app.js
@@ -2,5 +2,39 @@ const canvas = document.querySelector("canvas");
 canvas.width = 800;
 canvas.height = 800;
 const ctx = canvas.getContext("2d");
+ctx.lineWidth = 2;
+ctx.moveTo(0, 0);  
+let isPainting = false;
 
-ctx.fillRect(10,10,10,10);
+function onMove(event) {
+    if (isPainting) {
+        ctx.lineTo(event.offsetX, event.offsetY);   
+        ctx.stroke();
+        return;
+    }
+    ctx.moveTo(event.offsetX, event.offsetY);
+}
+
+function stopPainting(event) {
+    isPainting = false;
+}
+
+function startPainging(event) {
+    isPainting = true; 
+}
+
+//function onClick(event) {
+//    if (isPainting) {
+//        ctx.lineTo(event.offsetX, event.offsetY);   
+//        ctx.stroke();
+//};
+
+const colors = [
+    
+];
+
+canvas.addEventListener("mousemove", onMove);
+canvas.addEventListener("mousedown", startPainging);
+canvas.addEventListener("mouseup", stopPainting);
+canvas.addEventListener("mouseleave", stopPainting);
+
