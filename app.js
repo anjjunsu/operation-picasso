66f98035e1d9a7199e1558891448e3a2ba0560ad
Junsu An <anjjunsu@gmail.com>
2022-10-10 19:18:25 -0700

    Img upload feature

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const WIDTH = 800;
 const HEIGHT = 800;
 
+const imgInput = document.getElementById("img-upload");
 const eraseBtn = document.getElementById("erase-btn");
 const eraseAllBtn = document.getElementById("erase-all-btn");
 const modeBtn = document.getElementById("mode-btn");
@@ -19,6 +20,17 @@ ctx.moveTo(0, 0);
 let isPainting = false;
 let isFillMode = false;
 let isEraseMode = false;
+
+function onImgChange(event) {
+    const img = event.target.files[0];
+    const url = URL.createObjectURL(img); // Create url for img
+    const newImg = new Image();
+    newImg.src = url;
+    newImg.onload = () => {
+        ctx.drawImage(newImg, 0, 0, WIDTH, HEIGHT);
+        imgInput.value = null;
+    }
+}
 
 function onLineWidthChange(event) {
     ctx.lineWidth = event.target.value;
@@ -84,12 +96,6 @@ function startPainging(event) {
     isPainting = true; 
 }
 
-//function onClick(event) {
-//    if (isPainting) {
-//        ctx.lineTo(event.offsetX, event.offsetY);   
-//        ctx.stroke();
-//};
-
 const colors = [
     
 ];
@@ -106,3 +112,4 @@ color.addEventListener("change", onColorChange);
 modeBtn.addEventListener("click", onModeBtnClick);
 eraseAllBtn.addEventListener("click", onEraseAllBtnClick);
 eraseBtn.addEventListener("click", onEraseBtnClick);
+img.addEventListener("change", onImgChange);
