6e1debddd4490fb2bd667ed1852ffcaf8f7e5927
Junsu An <anjjunsu@gmail.com>
2022-10-10 18:42:36 -0700

    color fill mode feature

--- app.js
+++ app.js
@@ -1,15 +1,20 @@
+const WIDTH = 800;
+const HEIGHT = 800;
+
+const modeButton = document.getElementById("mode-button");
 const colorOptions = Array.from(
     document.getElementsByClassName("color-option")
 );
 const lineWidth = document.getElementById("line-width");
 const color = document.getElementById("color");
 const canvas = document.querySelector("canvas");
-canvas.width = 800;
-canvas.height = 800;
+canvas.width = WIDTH;
+canvas.height = HEIGHT;
 const ctx = canvas.getContext("2d");
 ctx.lineWidth = lineWidth.value;
 ctx.moveTo(0, 0);  
 let isPainting = false;
+let isFillMode = false;
 
 function onLineWidthChange(event) {
     ctx.lineWidth = event.target.value;
@@ -26,9 +31,10 @@ function onColorChange(event) {
 
 function onColorClick(event) {
     const chosenColor = event.target.dataset.color;
-    changeCtxColor(color); 
+    changeCtxColor(chosenColor); 
     color.value = chosenColor;
 }
+
 function onMove(event) {
     if (isPainting) {
         ctx.lineTo(event.offsetX, event.offsetY);   
@@ -36,6 +42,22 @@ function onMove(event) {
         return;
     }
     ctx.moveTo(event.offsetX, event.offsetY);
+}
+
+function onModeButtonClick() {
+    if (isFillMode) {
+        isFillMode = false;
+        modeButton.innerText = "Fill";
+    } else {
+        isFillMode = true;
+        modeButton.innerText = "Draw";
+    }
+}
+
+function onCanvasClick() {
+    if (isFillMode) {
+        ctx.fillRect(0, 0, WIDTH, HEIGHT);
+    }
 }
 
 function stopPainting(event) {
@@ -61,8 +83,9 @@ canvas.addEventListener("mousemove", onMove);
 canvas.addEventListener("mousedown", startPainging);
 canvas.addEventListener("mouseup", stopPainting);
 canvas.addEventListener("mouseleave", stopPainting);
+canvas.addEventListener("click", onCanvasClick);
 
 colorOptions.forEach(color => color.addEventListener("click", onColorClick));
 lineWidth.addEventListener("change", onLineWidthChange);
 color.addEventListener("change", onColorChange);
-
+modeButton.addEventListener("click", onModeButtonClick);
